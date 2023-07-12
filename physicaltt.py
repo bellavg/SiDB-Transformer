950c0e96609f103308ee21c1b009e104771d0bd9
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2023-07-12 20:19:39 +0200

    Update physicaltt.py

--- physicaltt.py
+++ physicaltt.py
@@ -7,19 +7,19 @@ from old.basetest import get_classification_report
 
 torch.cuda.empty_cache()
 
-logger = CSVLogger(save_dir='/home/igardner/', name='physical1logs')
+logger = CSVLogger(save_dir='/home/igardner/newresults/', name='physical1logs')
 # training loop
 checkpoint_callback2 = ModelCheckpoint(
     save_top_k=1,
     monitor="val_acc",
-    every_n_epochs=20,
+    every_n_epochs=10,
     mode="max",
-    dirpath="/home/igardner/physical1logs/",
+    dirpath="/home/igardner/newresults/physical1logs/",
     filename="bb",
 )
 
 
-trainer = pl.Trainer(max_epochs=EPOCHS, logger=logger, check_val_every_n_epoch=20, strategy='ddp_find_unused_parameters_true',
+trainer = pl.Trainer(max_epochs=EPOCHS, logger=logger, check_val_every_n_epoch=10, strategy='ddp_find_unused_parameters_true',
                      callbacks=[checkpoint_callback2], devices=2, accelerator="gpu")
 
 physical1model = LitModel(pe="physical1")
@@ -28,11 +28,11 @@ trainer.fit(model=physical1model, train_dataloaders=train_loader, val_dataloader
 filepath = '/home/igardner/physical1logs/physical1_model.pth'
 torch.save(physical1model.transformer.state_dict(), filepath)
 
-trainer.test(model=physical1model, dataloaders=test_loader, verbose=True, ckpt_path="/home/igardner/physical1logs/bb.ckpt")
+trainer.test(model=physical1model, dataloaders=test_loader, verbose=True, ckpt_path="/home/igardner/newresults/physical1logs/bb.ckpt")
 
 targets = torch.cat(physical1model.testtarget)
 predictions = torch.cat(physical1model.testpred)
 
 dataframe = get_classification_report(targets, predictions)
 
-dataframe.to_csv("/home/igardner/physical1logs/testclassificationreport.csv")
+dataframe.to_csv("/home/igardner/newresults/physical1logs/testclassificationreport.csv")
